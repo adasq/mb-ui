43217e1790b92454edc4c5e50e7f6d1110fb1962
Adam Płócieniak <adasq3@gmail.com>
2017-08-03 11:09:22 +0200

    .

--- src/pages/list/troopers/edit/edit.ts
+++ src/pages/list/troopers/edit/edit.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, ViewController } from 'ionic-angular';
- 
+
 import { Trooper } from '../../../../app/trooper/trooper.interface';
 import { Domain } from '../../../../app/lists/list.interface';
 import { ListsService } from '../../../../app/lists/lists.service';
@@ -21,17 +21,22 @@ export class ListTroopersEditPage {
     public params: NavParams,
     private listsService: ListsService
   ) {
-      this.trooper = this.params.get('trooper');
-      this.domain = this.params.get('domain');
-      const {name, pass} = this.trooper;
-      this.tempTrooper = {name, pass} as Trooper;
+    this.trooper = this.params.get('trooper');
+    this.domain = this.params.get('domain');
+    const { name, pass } = this.trooper;
+    this.tempTrooper = { name, pass } as Trooper;
   }
 
   public onStateChanged(state) {
-    if(state === STATE.SUCCESS) {
-        this.closeEditPage();
+    if (state === STATE.SUCCESS) {
+      this.closeEditPage();
     }
   }
+
+  public onDismissClick() {
+    this.viewCtrl.dismiss();
+  }
+
   private closeEditPage() {
     this.trooper.name = this.tempTrooper.name;
     this.trooper.pass = this.tempTrooper.pass;
